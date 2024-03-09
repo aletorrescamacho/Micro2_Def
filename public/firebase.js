fa6c1a924a40fd313b1310eebefda1ba7d929dbf
Soriano-1807 <131498101+Soriano-1807@users.noreply.github.com>
2024-03-09 03:58:47 +0000

    firebase perfecto.  firestore ya tiene clubes y juegos

---
+++ public/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDfitVQdsDZ5EHVB084u6fRYmTxAviUnB4",
+  authDomain: "micro2-5bbac.firebaseapp.com",
+  projectId: "micro2-5bbac",
+  storageBucket: "micro2-5bbac.appspot.com",
+  messagingSenderId: "90359441386",
+  appId: "1:90359441386:web:f436c561ade0541c83b493"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
